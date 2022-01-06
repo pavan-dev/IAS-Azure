7722b1623cfdd53f911bc1a327b9381e89b50538
pavan-dev <30730627+pavan-dev@users.noreply.github.com>
2022-01-07 00:08:33 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name     = "Jan2022"
+resource_group_location = "eastus"
+# azurerm_app_service_plan = "demo-plan"
+# azurerm_app_service      = "dev-webapp-Aug"
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  description = "name of RS"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "location of RG"
+  type        = string
+}
+
+# variable "azurerm_app_service_plan" {
+#   description = "new appserviceplan"
+#   type = string
+# }
+
+# variable "azurerm_app_service" {
+#   description = "name of web-app"
+#   type        = string
+# }
